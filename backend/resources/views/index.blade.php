e47458458e7ced5ad4cbf9f5af961e646f44da99
fumiemon <fumiaki0714s@gmail.com>
2022-04-04 01:42:00 +0900

    保存

---
+++ backend/resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>のんびりエンジニア</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+	<body>
+		<div class = "header">
+			<h1>のんびりエンジニア</h1>
+			<div class = "menu"><a href="profile">自己紹介</a></div>
+			<div class = "menu"><a href="">ポートフォリオ</a></div>
+			<div class = "menu"><a href="">お問い合わせ</a></div>
+		</div>
+		<div class = "body">
+			<p align = "center">このサイトはガジェット好きエンジニアのポートフォリオサイトです。<br></p>
+			<p align = "center">中学生の頃に作成した自作パソコンをはじめに、<br>スマートフォン、オーディオ、カメラ、パソコン周辺機器、、、<br></p>
+			<p align = "center">あらゆるガジェットに手を出して来ました。<br></p>
+			<p align = "center">ちなみにですが、スマートフォンは現在使っているので26台目です</P>
+			<p align = "center">ここでは一眼レフで撮影した写真をポートフォリオをとして掲載していますので、<br>ぜひ時間があるときに見てください。<br></p>
+		</div>
+		<div class = "footer">
+			<div class = "share">
+				<a href="https://line.me/ti/p/kCTOYbqulb"><img src = "img/LINEアイコン.png" width = "50" height = "50"></a>
+				<a href="https://www.instagram.com/sh1m01de/"><img src = "img/Instagramアイコン.png" width = "50" height = "50"></a>
+				<a href="https://twitter.com/sefia0714"><img src = "img/Twitterアイコン.png" width = "50" height = "50"></a>
+			</div>
+			<div class = "copyright">
+				<p><small>© 2022 FumiakiShimoide</small></p>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ backend/resources/views/profile.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>のんびりエンジニア</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+	<body>
+		<div class = "header">
+			<a href = "index"><h1>のんびりエンジニア</h1></a>
+			<div class = "sub-title"><a href="">自己紹介</a></div>
+		</div>
+		<div class = "body">
+			<p>名前
+				<ruby>
+					<rb>下出史明</rb><rp>（</rp><rt>しもいでふみあき</rt><rp>）</rp>
+				</ruby>
+			</p>
+			<p>年齢
+				24歳
+			</p>
+			<p>最終学歴
+				東京電子専門学校
+			</p>
+			<p>経験現場
+				<lu>
+					<li>ホテル業界　数理最適化による部屋割り最適化</li>
+					<li>人材業界　人材と企業のマッチングロジック</li>
+					<li>人材業界　募集サイト保守</li>
+				</lu>
+			</p>
+			<p>使用技術
+			<table border="1">
+				<tr>
+					<th>言語</th><th>使用年数</th>
+				</tr>
+				<tr>
+					<td>Java</td><td>2年</td>
+				</tr>
+				<tr>
+					<td>Python</td><td>3ヵ月</td>
+				</tr>
+			</table>
+			</P>
+		</div>
+		<div class = "footer">
+			<div class = "share">
+				<a href="https://line.me/ti/p/kCTOYbqulb"><img src = "img/LINEアイコン.png" width = "50" height = "50"></a>
+				<a href="https://www.instagram.com/sh1m01de/"><img src = "img/Instagramアイコン.png" width = "50" height = "50"></a>
+				<a href="https://twitter.com/sefia0714"><img src = "img/Twitterアイコン.png" width = "50" height = "50"></a>
+			</div>
+			<div class = "copyright">
+				<p><small>© 2022 FumiakiShimoide</small></p>
+			</div>
+		</div>
+	</body>
+</html>
